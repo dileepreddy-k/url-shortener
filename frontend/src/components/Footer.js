1d1c7f54391c3b6f022bffb761ead5de03f1cd67
dileepreddyk <karrolladileep@gmail.com>
2022-07-11 22:01:59 +0530

    Footer & Copy to clipboard

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import { Layout, Row, Col, Typography } from 'antd';
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+            <Layout.Footer>
+                <Row gutter={[16, 16]} align='middle' justify='space-between'>
+                    <Col>
+                        <Typography.Paragraph style={{margin: 0}}>URL Shortner</Typography.Paragraph>
+                    </Col>
+                    <Col>
+                        <Typography.Paragraph style={{margin: 0}}>&copy; 2022</Typography.Paragraph>
+                    </Col>
+                </Row>
+            </Layout.Footer>
+        </>
     )
 }
 
